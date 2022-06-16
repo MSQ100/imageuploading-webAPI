adfbe8591dc0420d59a2cc3a24ca9d436b54b6e6
mohammad <mohammadsabir456@gmail.com>
2022-06-16 06:49:53 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'imageupload';
+  constructor(private httpClient: HttpClient) { }
+
+  uploadedImage!: File;
+  dbImage: any;
+  postResponse: any;
+  successResponse!: string;
+  image: any;
+
+  public onImageUpload(event:any) {
+    this.uploadedImage = event.target.files[0];
+  }
+
+
+  imageUploadAction() {
+    const imageFormData = new FormData();
+    imageFormData.append('image', this.uploadedImage, this.uploadedImage.name);
+
+
+    this.httpClient.post('http://localhost:8080/upload/image/', 
+    imageFormData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.postResponse = response;
+          this.successResponse = this.postResponse.body.message;
+        } else {
+          this.successResponse = 'Image not uploaded due to some error!';
+        }
+      }
+      );
+    }
+
+  viewImage() {
+    this.httpClient.get('http://localhost:8080/get/image/info/' + this.image)
+      .subscribe(
+        res => {
+          this.postResponse = res;
+          this.dbImage = 'data:image/jpeg;base64,' + this.postResponse.image;
+        }
+      );
+  }
 }
